54034f427e646e861633a2172a8fff1c24aea512
Crystal Wang <crystalmaro@gmail.com>
2019-06-23 19:15:20 +0800

    fix assign 5 for loop syntax error

--- week-2/w2a4.js
+++ week-2/w2a4.js
@@ -104,8 +104,8 @@ button.addEventListener("click", function(){
 // Assignment 5: (Advanced Optional)
 function twoSum(nums, target){
     let result = [];
-    for (let i = 0; i < nums.length, i++) {
-        for (let j = 0; j < nums.length, j++) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
                 result.push(i);
                 result.push(j);
