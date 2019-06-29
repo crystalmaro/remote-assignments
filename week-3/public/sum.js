7a97d433a3c19906408c5fe161e5c72d62dbc984
Crystal Wang <crystalmaro@gmail.com>
2019-06-29 16:33:42 +0800

    fix closing brackets

--- week-3/public/sum.js
+++ week-3/public/sum.js
@@ -1,21 +1,21 @@
- // Week 3 - Assign 3
- // 1. create XMLHTTP Request object
- let xhr = new XMLHttpRequest();
- // 2. create callback function
- xhr.onreadystatechange = function () {
-     if (this.readyState === 4 && this.status === 200) {
-         document.querySelector("#ajax").innerHTML = this.responseText;
+// Week 3 - Assign 3
+// 1. create XMLHTTP Request object
+let xhr = new XMLHttpRequest();
+// 2. create callback function
+xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        document.querySelector("#ajax").innerHTML = this.responseText;
+        }
+    };
+// 3. open a request
+function sendAJAX() {
+    let numberInput = document.getElementById("numberInput").value
+    let url = "/getData";
+    if (numberInput !== '') {
+        url += "?number=";
      }
- };
- // 3. open a request
- function sendAJAX() {
-     let numberInput = document.getElementById("numberInput").value
-     let url = "/getData";
-     if (numberInput !== '') {
-         url += "?number=";
-     }
-     xhr.open("GET", url + numberInput);
-     // you make an open req per send
-     // 4. send a request
-     xhr.send();
- }
+    xhr.open("GET", url + numberInput);
+    // you make an open req per send
+    // 4. send a request
+    xhr.send();
+};
