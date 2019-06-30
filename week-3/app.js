c04444b3cffa917088d87ea3b8562c1e21b44742
Crystal Wang <crystalmaro@gmail.com>
2019-06-30 19:35:22 +0800

    clean up code

--- week-3/app.js
+++ week-3/app.js
@@ -15,32 +15,6 @@ const mainRoutes = require("./routes/index");
 app.use(mainRoutes);
 // links routes (index.js) to this file (app.js)
 
-// paul's 
-// const cookieRoutes = require("./routes/cookies");
-// app.use(cookieRoutes);
-// app.get('/cookies', function (req, res) {
-//     res.send(JSON.stringify(req.cookies));
-// });
-
-// app.listen(port, function () {
-//     console.log('cookie-parser demo is up on port: ' + port);
-// });
-
-/*
-// treehouse cookies
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
-app.get("/", function (req, res) {
-    // Cookies that have not been signed
-    console.log("Cookies: ", req.cookies);
-
-    res.send(JSON.stringify(req.cookies));
-  
-    // Cookies that have been signed
-    console.log("Signed Cookies: ", req.signedCookies);
-  });
-*/
-
 // set up developmen server using the LISTEN method
 // it creates a server, when it's run on local machine
 // it sends request through a special URL called localhost
