ee0a46ca025be28b92203dc01b685185e7a35780
Crystal Wang <crystalmaro@gmail.com>
2019-06-30 19:36:23 +0800

    add assign4 /trackName cookie for myName.js

--- week-3/routes/index.js
+++ week-3/routes/index.js
@@ -10,19 +10,21 @@ res.send("Hey, crystal's server is depending on express!"))
 
 // === Assignment 2
 router.get("/getData", function(req, res){
-    let number = Number(req.query.number);
-    if (Object.keys(req.query).length === 0) {
+    let number = parseInt(req.query.number);
+    if (!req.query || req.query.number === undefined) {
+        // Object.keys(req.query).length === 0
         //req.query is number in getData?number
         res.send("Lack of Parameter");
-    } else if (!number) {
+    } else if (isNaN(number) || number <= 0) {
         res.send("Wrong Parameter");
     } else {
         let result = 0;
         for (let i = 1; i <= number; i++) {
             result += i;
         }
-        res.send("" + result);
+        res.send(result.toString());
         // adding "" makes the result to string instead of number
+        // "" + result can also be replaced by result.toString()
         // otherwise the app recognizes result number as status code
     } 
 });
@@ -32,25 +34,10 @@ router.get("/trackName", (req, res) => {
     let name = (req.query.name);
     if (name !== "") {
         res.cookie("name", name);
+        res.redirect("/myName");
         res.send(name);
     }
 });
-
-// router.get("/trackName", (req, res) => {
-//     const name = req.cookies.name;
-//     if (name) {
-//         res.redirect("/trackName");
-//     } else {
-//         res.render("hello"); 
-//     }
-// });
-
-// router.post("/trackName", (req, res) => {
-//     res.cookie("username", name);
-    
-//     res.redirect("/trackName");
-// });
-// ======== 
 
 // exports this so app.js can import
 module.exports = router;
