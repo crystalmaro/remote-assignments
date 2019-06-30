2fbe2cb7fb55d726fafa43d9afa8eb3f12bf5d6d
Crystal Wang <crystalmaro@gmail.com>
2019-06-30 19:32:39 +0800

    put text input into div

--- week-3/public/myName.js
+++ week-3/public/myName.js
@@ -1,31 +1,31 @@
 // Week 3 - Assign 4
 
-// 1. create XMLHTTP Request object
-let xhr = new XMLHttpRequest();
-// 2. create callback function
-xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        document.querySelector("#ajax").innerHTML = this.responseText;
-    }
-};
-// @todo parse cookies to return name value
-// currently name=string
-// https://firstdoit.com/quick-tip-one-liner-cookie-read-c695ecb4fe59
-let allCookies = document.cookie;
-if (allCookies !== "") {
-    document.querySelector("#ajax").innerHTML = allCookies;
-};
-
-// 3. open a request
+var dc = document.cookie;
+var begin = dc.indexOf('name=')
+if (begin != -1) {
+    var name = dc.substring(begin+5)
+    document.getElementById("ajax").innerHTML = name;
+    document.getElementById("nameBox").style.display = "none";
+    // hide input box if 
+}
+// 3. open a request when button is clicked
 function trackName() {
-    // @todo check cookies for names
+    // 1. create XMLHTTP Request object
+    let xhr = new XMLHttpRequest();
+    // 2. create callback function
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            //both window.locaiton and location work below
+            location.href = this.responseURL;
+            // location.reload();
+        }
+    };
     let user = document.getElementById("getName").value
     let url = "/trackName";
     if (user !== '') {
         url += "?name=" + user;
     };
     xhr.open("GET", url);
-    // you make an open req per send
     // 4. send a request
     xhr.send();
 };
